2d84c745678a89da003f56d0b9b07fe9181e382a
usuario <usuario@DESKTOP-LATITUD>
2022-10-30 22:15:01 +0100

    Agregar archivos de proyecto.

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.nUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcular suma desde 1hasta: ";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(38, 108);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(229, 199);
+            this.listBox.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(38, 73);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(148, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Ver resultados parcieles";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // nUpDown
+            // 
+            this.nUpDown.Location = new System.Drawing.Point(203, 28);
+            this.nUpDown.Name = "nUpDown";
+            this.nUpDown.Size = new System.Drawing.Size(64, 23);
+            this.nUpDown.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nUpDown);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox;
+        private CheckBox checkBox1;
+        private NumericUpDown nUpDown;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = (int)nUpDown.Value;
+            int suma=0;
+            int i;
+
+            for (i = 0; i < num+1; i++)
+            {
+                suma+=i;
+                listBox.Items.Add("Sumando: " + i + " Suma parcial: " + suma.ToString());
+            }
+           
+            listBox.Items.Add("La suma TOTAL es: "+suma.ToString());
+        }
+    }
+}
